ddeca90f745ece8e37ca2424b3da27850df12c1c
Masankwa <masankwacornelius@gmail.com>
2023-07-13 12:54:21 -0700

    firstc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - iprints a line with a function
+ * Return: 0 (done)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
