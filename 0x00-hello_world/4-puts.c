c05689f9ef53aee7c42bca9ed65b0169d711184e
Masankwa <masankwacornelius@gmail.com>
2023-07-13 13:23:41 -0700

    hopeee aki

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - iprints a line with a function
- * Return: 0 (done)
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
